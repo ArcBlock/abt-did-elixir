98d9574a8fa3e616c5c77f8d532379eb7d2e064d
Peiling Ding <dingpl716@gmail.com>
2019-02-21 14:35:41 -0800

    Rename to abt-did-elixir (#7)
    * Rename to abt-did-elixir
    
    * bump version

--- .makefiles/elixir.mk
+++ .makefiles/elixir.mk
@@ -5,7 +5,7 @@ TMP_OTP_PATH=/tmp/esl_otp_$(OTP_VERSION)
 TMP_OTP_FILE=$(TMP_OTP_PATH)/otp_$(OTP_VERSION).rpm
 
 copy-docs: $(RELEASE_DIR)
-	@cd $(OUTPUT_DIR); tar zcf abt_did_docs.tgz *; mv abt_did_docs.tgz ../_build/releases/;
+	@cd $(OUTPUT_DIR); tar zcf abt_did_elixir_docs.tgz *; mv abt_did_elixir_docs.tgz ../_build/releases/;
 
 build-dev:
 	@echo "Building the dev release..."
